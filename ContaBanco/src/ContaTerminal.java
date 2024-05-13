b9af4f90a9651d1d03d79715dd6817ff3dd463b7
LittleDustDevWIN <markraph19@gmail.com>
2024-05-13 11:31:24 -0300

    feat: add arquivo java

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo: ");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível " + 
+        "para saque.");
+
+    }
+}
